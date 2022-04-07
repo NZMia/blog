0ad5a1596db5da823082d546d36e3f705bb388d0
Mia.Zhang <mia7zwy@hotmail.com>
2022-04-07 22:27:33 +1200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles/main.scss'
+import cloth from './assets/1.jpeg'
+
+const img = document.getElementById('cloth');
+img.src = cloth
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports ={
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: '[name][ext]',
+        clean: true
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 5050,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpeg|jpg)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+                title: 'blog',
+                filename: 'index.html',
+                template: 'src/index.html'
+            }
+        ),
+        new BundleAnalyzerPlugin()
+    ]
+}
