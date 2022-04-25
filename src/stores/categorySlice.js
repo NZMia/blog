9dd36f977affd410612ab6788cf03f1c98e0ea5c
Mia.Zhang <mia7zwy@hotmail.com>
2022-04-25 14:55:48 +1200

    fix: remove testing code

--- src/stores/categorySlice.js
+++ src/stores/categorySlice.js
@@ -16,38 +16,7 @@ export const categorySlice = createSlice({
   name: 'category',
 
   initialState: {
-    categoryItems: [
-      {
-        title: 'CSS',
-        icon: 'https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg',
-        id: 1,
-      },
-      {
-        title: 'JavaScript',
-        icon: 'https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg',
-        id: 2,
-      },
-      {
-        title: 'React JS',
-        icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
-        id: 3,
-      },
-      {
-        title: 'Webpack',
-        icon: 'https://raw.githubusercontent.com/webpack/media/master/logo/icon.png',
-        id: 4,
-      },
-      {
-        title: 'Redux Toolkit',
-        icon: 'https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg',
-        id: 5,
-      },
-      {
-        title: 'Test Library',
-        icon: 'https://testing-library.com/img/octopus-64x64.png',
-        id: 6,
-      },
-    ],
+    categoryItems: [],
   },
 
   extraReducers: {
